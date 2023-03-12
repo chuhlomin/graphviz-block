07a5406e657297c3aebb43bcbdd1e96489ee1d61
Konstantin Chukhlomin <mail@chuhlomin.com>
2023-03-12 19:35:19 -0400

    feat: render SVG from dot file

---
+++ blocks/graphviz-block/index.tsx
@@ -0,0 +1,35 @@
+import { FileBlockProps } from "@githubnext/blocks";
+import { Box } from "@primer/react";
+import graphviz from "graphviz-wasm";
+import { useState, useEffect } from "react";
+
+export default function GrpahvizFileBlock(props: FileBlockProps) {
+  const { content } = props;
+
+  const [svg, setSvg] = useState('');
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    graphviz.loadWASM()
+      .then(() => {
+        console.log("loaded wasm");
+        setSvg(graphviz.layout(content));
+      })
+      .catch((error) => {
+        console.error(error);
+        setError('error');
+      })
+  }, []);
+
+  if (error !== "") {
+    return (
+        <h1>Error</h1>
+    );
+  }
+
+  return (
+    <Box p={4}>
+      <svg className="graphviz" dangerouslySetInnerHTML={{ __html: svg }} />
+    </Box>
+  );
+}
